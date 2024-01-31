0089564468eb02118512243978e0545e90862a37
etherealhero <gutcan4312@gmail.com>
2024-01-31 22:47:45 +1000

    bottom navbar

--- afs_cappuccino/lib/dashboard_page.dart
+++ afs_cappuccino/lib/dashboard_page.dart
@@ -13,10 +13,42 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  int _currentTab = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorPalette().scaffoldBg,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: ColorPalette().scaffoldBg,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: ColorPalette().accentColor,
+        unselectedItemColor: ColorPalette().secondaryColor,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _currentTab,
+        onTap: (value) => setState(() {
+          _currentTab = value;
+        }),
+        items: const [
+          BottomNavigationBarItem(
+            label: "home",
+            icon: Icon(Icons.home_rounded, size: 28.0),
+          ),
+          BottomNavigationBarItem(
+            label: "shopping_bag",
+            icon: Icon(Icons.breakfast_dining_rounded, size: 28.0),
+          ),
+          BottomNavigationBarItem(
+            label: "heart",
+            icon: Icon(Icons.favorite_sharp, size: 28.0),
+          ),
+          BottomNavigationBarItem(
+            label: "bell",
+            icon: Icon(Icons.notifications_rounded, size: 28.0),
+          ),
+        ],
+      ),
       body: const SingleChildScrollView(
           child: Column(
         children: <Widget>[
