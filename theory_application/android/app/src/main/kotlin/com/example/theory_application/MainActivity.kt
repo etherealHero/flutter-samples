7255a04cd4e3f9c46c16b62f494dc9f89d99bab2
etherealhero <gutcan4312@gmail.com>
2024-03-29 21:16:22 +1000

    new sample

---
+++ theory_application/android/app/src/main/kotlin/com/example/theory_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.theory_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ theory_application/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'src/home_page.dart';
+
+void main() {
+  runApp(const MyHomePage());
+}
